69ab9ae2924b02b2cc183fde1b33d6486e0f090f
Joris Strakeljahn <j.strakeljahn.vkg@hoermann.de>
2024-09-13 18:53:29 +0200

    Initialer Commit

---
+++ src/main/java/de/hsbi/binex/binex_backend/BinexBackendApplication.java
@@ -0,0 +1,13 @@
+package de.hsbi.binex.binex_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinexBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinexBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hsbi/binex/binex_backend/BinexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsbi.binex.binex_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
